dee451237e1997326f99810958ad3900b109994b
Naki <leandroreisubi@gmail.com>
2024-08-24 22:31:55 -0300

    Auth

--- src/main/java/com/example/pizzaio/CreateUserScreen.java
+++ src/main/java/com/example/pizzaio/CreateUserScreen.java
@@ -1,6 +1,8 @@
 package com.example.pizzaio;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -18,13 +20,16 @@ import javafx.stage.Stage;
 public class CreateUserScreen {
 
     @FXML
-    private Button createButton;
+    private Button criarUsuarioButton;
 
     @FXML
-    private TextField usuarioTextField;
+    private TextField userField;
 
     @FXML
-    private PasswordField senhaTextField;
+    private PasswordField passwordField;
+
+    @FXML
+    private Button voltarButton;
 
     private Stage stage;
     private static final String CREDENTIALS_FILE = "users.txt";
@@ -36,7 +41,7 @@ public class CreateUserScreen {
     public void showCreateUserScreen() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CreateUserScreenLayout.fxml"));
-            loader.setController(this); // Define this class as the controller
+            loader.setController(this); // Define esta classe como o controlador
             Parent root = loader.load();
 
             // Configurar a cena
@@ -45,8 +50,9 @@ public class CreateUserScreen {
             stage.setScene(scene);
             stage.show();
 
-            //Configurar a ação do botão de criar
-            createButton.setOnAction(e -> handleCreate(usuarioTextField.getText(), senhaTextField.getText()));
+            // Configurar as ações dos botões
+            criarUsuarioButton.setOnAction(e -> handleCreate(userField.getText(), passwordField.getText()));
+            voltarButton.setOnAction(e -> voltarButtonAction());
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -55,8 +61,12 @@ public class CreateUserScreen {
 
     private void handleCreate(String username, String password) {
         if (isValidCredentials(username, password)) {
-            createNewUser(username, password);
-            JOptionPane.showMessageDialog(null, "Usuário criado com sucesso.");
+            if (isUsernameDuplicate(username)) {
+                JOptionPane.showMessageDialog(null, "Nome de usuário já existe. Escolha outro.");
+            } else {
+                createNewUser(username, password);
+                JOptionPane.showMessageDialog(null, "Usuário criado com sucesso.");
+            }
         } else {
             JOptionPane.showMessageDialog(null, "Usuário ou senha inválidos.");
         }
@@ -73,5 +83,25 @@ public class CreateUserScreen {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean isUsernameDuplicate(String username) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(CREDENTIALS_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] credentials = line.split(":");
+                if (credentials[0].equals(username)) {
+                    return true; // Retorna true se encontrar um nome de usuário duplicado
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false; // Retorna false se o nome de usuário não for duplicado
+    }
+
+    private void voltarButtonAction() {
+        LoginScreen loginScreen = new LoginScreen(stage);
+        loginScreen.showLoginScreen();
     }
 }
